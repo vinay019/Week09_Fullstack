1c414af33f66cfcf311e880d6aaa65bd28971b0a
Vinay Raveendran <vinay.019@gmail.com>
2025-08-04 11:40:56 +0100

    some updates to the server.js file- Testing

--- server/server.js
+++ server/server.js
@@ -11,12 +11,23 @@ dotenv.config();
 
 console.log("my secret env is", process.env.my_Secret_Key);
 
+console.log("Second", process.env.my_Second_Key);
+
+console.log("third", process.env.my_thirdKEY);
+
 const PORT = 8080;
 
 app.get("/", function (req, res) {
   res.send("Ouch, you've hit my root!!!");
 });
 
+app.get("/secrets", function (req, res) {
+  console.log("my secret env is", process.env.my_Secret_Key);
+
+  console.log("Second", process.env.my_Second_Key);
+
+  console.log("third", process.env.my_thirdKEY);
+});
 app.listen(PORT, function () {
   console.log(`Server is running on ${PORT}`);
 });

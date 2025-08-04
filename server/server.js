f7b347ccfdbb037b5fc8bf0aa86729b699aa62bd
Vinay Raveendran <vinay.019@gmail.com>
2025-08-04 11:13:25 +0100

    chore:init repo with server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+console.log("my secret env is", process.env.my_Secret_Key);
+
+const PORT = 8080;
+
+app.get("/", function (req, res) {
+  res.semd("Ouch, you've hit my root!!!");
+});
+
+app.listen(PORT, function () {
+  console.log(`Server is running on ${PORT}`);
+});
